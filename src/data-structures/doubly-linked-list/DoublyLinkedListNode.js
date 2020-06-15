e4dc5a30566a67662852870d5d0cef4cc8961a29
Lekshmi Chandra <lekshmi.chandra90@gmail.com>
2020-06-15 07:50:15 +0200

    doubly linked list - toarray, tostring, append, prepend

--- src/data-structures/doubly-linked-list/DoublyLinkedListNode.js
+++ src/data-structures/doubly-linked-list/DoublyLinkedListNode.js
@@ -1,3 +1,15 @@
+// export default class DoublyLinkedListNode {
+//   constructor(value, next = null, previous = null) {
+//     this.value = value;
+//     this.next = next;
+//     this.previous = previous;
+//   }
+
+//   toString(callback) {
+//     return callback ? callback(this.value) : `${this.value}`;
+//   }
+// }
+
 export default class DoublyLinkedListNode {
   constructor(value, next = null, previous = null) {
     this.value = value;
